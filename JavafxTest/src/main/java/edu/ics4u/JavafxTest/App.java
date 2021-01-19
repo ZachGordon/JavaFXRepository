bbfd94db1fe08d2d4067885f0baaa37812ef0d91
gordz <gordz@ZachPC.phub.net.cable.rogers.com>
2021-01-19 15:47:21 -0500

    Update 2

--- JavafxTest/src/main/java/edu/ics4u/JavafxTest/App.java
+++ JavafxTest/src/main/java/edu/ics4u/JavafxTest/App.java
@@ -2,23 +2,24 @@ package edu.ics4u.JavafxTest;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    	var label = new Label("Nuclear launch button");
+        var button = new Button("Do Not Touch");
+        
+        var root = new VBox(label, button);
+        
+        var scene = new Scene(root, 200, 100);
         stage.setScene(scene);
         stage.show();
     }
@@ -26,5 +27,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
